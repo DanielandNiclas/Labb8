6df2cd680bd4fff670dee8975f56c05bfb700d83
Niclas Hjulström <niclas@nhmdev.se>
2016-11-08 19:48:04 +0100

    Deleted String Class because it has no use. Changed and commented lots of code. Changed so we make a instance of runtime and delegate is there. Also maked concat method private.

--- Labb8/Program.cs
+++ Labb8/Program.cs
@@ -8,33 +8,15 @@ namespace Labb8
 {
     class Program
     {
-        public delegate string StringConcatinator(string[] stringArray, char seperator);
+        
         static void Main(string[] args)
         {
-            //StringClass stringclass = new StringClass();
-            StringConcatinator del1 = new StringConcatinator(concat);
+            
+            Runtime runtime = new Runtime(); // Make a new instance of Runtime
+            runtime.Start();    // Run the method Start
 
-            string[] stringArray = new string[3] { "Niclas", "Daniel", "John" };
-
-
-            // var runtime = new Runtime();
-            // runtime.Start();
-
-            Console.WriteLine(del1(stringArray, ','));
-
-
+            Console.ReadKey(); // Just to stop the program to not exit without input.
         }
-        public static string concat(string[] stringArray, char seperator)
-        {
-            string ret = "";
-
-            foreach (string s in stringArray)
-            {
-                ret += s + seperator;
-            }
-            ret = ret.Remove(ret.Length - 1);
-            return ret;
-
-        }
+        
     }
 }
--- Labb8/Runtime.cs
+++ Labb8/Runtime.cs
@@ -10,12 +10,44 @@ namespace Labb8
 
     class Runtime
     {
-      
+        public delegate string DStringConcatinator(string[] stringArray, char separator);
 
         public void Start()
         {
-             
-    }
+            // Define a string array
+            string[] stringArray = new string[3] { "Niclas", "Daniel", "John" };
+
+            // Make a new instance of the string delegator that uses the concat method.
+            DStringConcatinator del1 = new DStringConcatinator(concat);
+
+            // Just write the result to console for first implementation.
+            Console.WriteLine(del1(stringArray, ','));  // This cw calls the delegate and sends our stringArray and separator as parameters.
+
+
+        }
+
+        private string concat(string[] stringArray, char separator)
+        {
+            // This class concatinates all the strings in stringArray with separator.
+            // Returns: string
+            // Method is private because delegate uses it and should not be used outside this class.
+
+            string retString = ""; // A string variable that is empty at start but will contain full return string at the end.
+
+            foreach (string s in stringArray)
+            {
+                // For each string in the string array...
+                
+                retString += s + separator; // Add the string to retString variable and add the separator to the end.
+            }
+
+            // When we have added all strings we have one separator too much in the retString.
+           
+            retString = retString.Remove(retString.Length - 1);  // Delete the last char in retString.
+            
+            return retString; // Return our completed retString.
+
+        }
     }
 
 }
