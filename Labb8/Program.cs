ceb850faff5c5c96ab9b09becd6574a76a1542ba
Daniel Rosenberg <dan.vic.ros@gmail.com>
2016-11-08 14:30:19 +0100

    first version

---
+++ Labb8/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb8
+{
+    class Program
+    {
+     
+        static void Main(string[] args)
+        {
+            StringClass stringclass = new StringClass();
+
+            string[] stringArray = new string[3] { "Niclas", "Daniel", "John" };
+
+
+            // var runtime = new Runtime();
+            // runtime.Start();
+
+            Console.WriteLine(stringclass.Concat(stringArray, ','));
+        }
+    }
+}
---
+++ Labb8/StringClass.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Labb8
+{
+    class StringClass
+    {
+
+        public string Concat(string[] stringArray, char seperator)
+        {
+            string ret = "";
+
+            foreach (string s in stringArray)
+            {
+                ret += s + seperator;
+            }
+            ret = ret.Remove(ret.Length - 1);
+            return ret;
+
+        }
+
+
+    }
+}
