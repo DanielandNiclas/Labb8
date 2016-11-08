aa724a9df386069742c8459f1ac5dfdc81a9c5f6
Niclas Hjulström <niclas@nhmdev.se>
2016-11-08 21:09:42 +0100

    Added float delegates. Removed FloatClass because its not needed.

--- Labb8/Program.cs
+++ Labb8/Program.cs
@@ -14,7 +14,7 @@ namespace Labb8
             
             Runtime runtime = new Runtime(); // Make a new instance of Runtime
             runtime.Start();    // Run the method Start
-
+            runtime.StartNumber();
             Console.ReadKey(); // Just to stop the program to not exit without input.
         }
         
--- Labb8/Runtime.cs
+++ Labb8/Runtime.cs
@@ -6,11 +6,11 @@ using System.Threading.Tasks;
 
 namespace Labb8
 {
-
-
     class Runtime
     {
         public delegate string DStringConcatinator(string[] stringArray, char separator);
+
+        public delegate float DFloatOperator(float[] floatArray);
 
         public void Start()
         {
@@ -24,6 +24,20 @@ namespace Labb8
             Console.WriteLine(del1(stringArray, ','));  // This cw calls the delegate and sends our stringArray and separator as parameters.
 
 
+        }
+
+        public void StartNumber()
+        {
+            // Define a float array
+            float[] floatArray = new float[3] { 5.5F, 3.5F, 25.1F };
+
+            // Make a new instances of the float delegators.
+            DFloatOperator FloatAdd = new DFloatOperator(floatAddMethod);
+            DFloatOperator FloatMultiply = new DFloatOperator(floatMultiplyMethod);
+
+            // Write the results to screen
+            Console.WriteLine(FloatAdd(floatArray).ToString());
+            Console.WriteLine(FloatMultiply(floatArray).ToString());
         }
 
         private string concat(string[] stringArray, char separator)
@@ -48,6 +62,39 @@ namespace Labb8
             return retString; // Return our completed retString.
 
         }
+
+        private float floatAddMethod(float[] floatArray)
+        {
+            float retFloat = 0F;
+            foreach(float f in floatArray)
+            {
+                retFloat += f;
+            }
+            return retFloat;
+        }
+
+        private float floatMultiplyMethod(float[] floatArray)
+        {
+            // This class multiplying all the floats in floatArray.
+            // Returns: float
+            // Method is private because delegate uses it and should not be used outside this class.
+
+            float retFloat = floatArray[0]; // Start with first float
+
+            // Start a for loop with i = 1 because thats our two first numbers. 
+            for (int i = 1; i < floatArray.Length; i++)
+            {
+                Console.Write("Multiplying " + retFloat.ToString() + " and " +floatArray[i].ToString());
+
+                retFloat *= floatArray[i];  // Multiply retFloat with next float from floatArray
+
+                Console.WriteLine(" = " +retFloat.ToString());
+            }
+
+            return retFloat; // Return our completed float result
+
+        }
+
     }
 
 }
